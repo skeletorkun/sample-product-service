659eb8f2c6be8c0a8e49d4fe5c03483494d5e2a3
Orkun Ozen <skeletorkun@gmail.com>
2020-02-17 22:10:36 +0100

    first commit

---
+++ src/test/java/com/company/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
